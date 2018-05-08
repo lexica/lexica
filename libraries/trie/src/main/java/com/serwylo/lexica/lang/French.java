6d3aebb2df003dc669f65683b1015d22d39f1ea8
Peter Serwylo <peter@serwylo.com>
2018-05-08 15:18:19 +1000

    Added Scrabble scores for French language

--- libraries/trie/src/main/java/com/serwylo/lexica/lang/French.java
+++ libraries/trie/src/main/java/com/serwylo/lexica/lang/French.java
@@ -11,43 +11,48 @@ public class French extends Language {
     static {
         letterPoints.put("à", 1);
         letterPoints.put("a", 1);
-        letterPoints.put("b", 1);
         letterPoints.put("â", 1);
-        letterPoints.put("c", 1);
-        letterPoints.put("d", 1);
         letterPoints.put("e", 1);
-        letterPoints.put("f", 1);
-        letterPoints.put("g", 1);
-        letterPoints.put("ç", 1);
         letterPoints.put("è", 1);
-        letterPoints.put("h", 1);
         letterPoints.put("i", 1);
         letterPoints.put("é", 1);
-        letterPoints.put("j", 1);
         letterPoints.put("ê", 1);
-        letterPoints.put("k", 1);
         letterPoints.put("ë", 1);
-        letterPoints.put("l", 1);
-        letterPoints.put("m", 1);
         letterPoints.put("n", 1);
         letterPoints.put("î", 1);
-        letterPoints.put("o", 1);
         letterPoints.put("ï", 1);
-        letterPoints.put("p", 1);
-        letterPoints.put("q", 1);
+        letterPoints.put("o", 1);
         letterPoints.put("r", 1);
         letterPoints.put("s", 1);
-        letterPoints.put("t", 1);
         letterPoints.put("ô", 1);
-        letterPoints.put("u", 1);
-        letterPoints.put("v", 1);
         letterPoints.put("ö", 1);
-        letterPoints.put("w", 1);
-        letterPoints.put("x", 1);
-        letterPoints.put("y", 1);
-        letterPoints.put("z", 1);
+        letterPoints.put("l", 1);
+        letterPoints.put("t", 1);
+        letterPoints.put("u", 1);
         letterPoints.put("û", 1);
         letterPoints.put("ü", 1);
+
+        letterPoints.put("d", 2);
+        letterPoints.put("g", 2);
+        letterPoints.put("m", 2);
+
+        letterPoints.put("b", 3);
+        letterPoints.put("c", 3);
+        letterPoints.put("ç", 3);
+        letterPoints.put("p", 3);
+
+        letterPoints.put("h", 4);
+        letterPoints.put("f", 4);
+        letterPoints.put("v", 4);
+
+        letterPoints.put("j", 8);
+        letterPoints.put("q", 8);
+
+        letterPoints.put("k", 10);
+        letterPoints.put("w", 10);
+        letterPoints.put("x", 10);
+        letterPoints.put("y", 10);
+        letterPoints.put("z", 10);
     }
 
     @Override
