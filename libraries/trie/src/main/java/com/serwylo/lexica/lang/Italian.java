69a151a098fccca295a22c5e4c363469e7677f1e
Peter Serwylo <peter@serwylo.com>
2018-05-08 21:52:52 +1000

    Added Scrabble scoring for Italian language. Also q has a u suffix in Italian.

--- libraries/trie/src/main/java/com/serwylo/lexica/lang/Italian.java
+++ libraries/trie/src/main/java/com/serwylo/lexica/lang/Italian.java
@@ -9,38 +9,47 @@ public class Italian extends Language {
     private static Map<String, Integer> letterPoints = new HashMap<>();
 
     static {
+        letterPoints.put("o", 1);
+        letterPoints.put("ò", 1);
         letterPoints.put("à", 1);
         letterPoints.put("a", 1);
-        letterPoints.put("b", 1);
-        letterPoints.put("c", 1);
-        letterPoints.put("d", 1);
-        letterPoints.put("e", 1);
-        letterPoints.put("f", 1);
-        letterPoints.put("g", 1);
-        letterPoints.put("h", 1);
-        letterPoints.put("è", 1);
         letterPoints.put("i", 1);
+        letterPoints.put("ì", 1);
+        letterPoints.put("e", 1);
+        letterPoints.put("è", 1);
         letterPoints.put("é", 1);
+
+        letterPoints.put("c", 2);
+        letterPoints.put("r", 2);
+        letterPoints.put("s", 2);
+        letterPoints.put("t", 2);
+
+        letterPoints.put("l", 3);
+        letterPoints.put("m", 3);
+        letterPoints.put("n", 3);
+        letterPoints.put("u", 3);
+        letterPoints.put("ù", 3);
+
+        letterPoints.put("d", 5);
+        letterPoints.put("b", 5);
+        letterPoints.put("f", 5);
+        letterPoints.put("p", 5);
+        letterPoints.put("v", 5);
+
+        letterPoints.put("g", 8);
+        letterPoints.put("h", 8);
+        letterPoints.put("z", 8);
+
+        letterPoints.put("q", 10);
+
+        // According to https://en.wikipedia.org/wiki/Scrabble_letter_distributions#Italian:
+        //   "The letters J, K, W, X, and Y are absent since these letters do not exist in Italian
+        //    and are only used in loanwords"
         letterPoints.put("j", 1);
         letterPoints.put("k", 1);
-        letterPoints.put("l", 1);
-        letterPoints.put("ì", 1);
-        letterPoints.put("m", 1);
-        letterPoints.put("n", 1);
-        letterPoints.put("o", 1);
-        letterPoints.put("p", 1);
-        letterPoints.put("q", 1);
-        letterPoints.put("r", 1);
-        letterPoints.put("ò", 1);
-        letterPoints.put("s", 1);
-        letterPoints.put("t", 1);
-        letterPoints.put("u", 1);
-        letterPoints.put("v", 1);
         letterPoints.put("w", 1);
         letterPoints.put("x", 1);
-        letterPoints.put("ù", 1);
         letterPoints.put("y", 1);
-        letterPoints.put("z", 1);
     }
 
     @Override
@@ -60,11 +69,19 @@ public class Italian extends Language {
 
     @Override
     public String toDisplay(String value) {
+        if (value.equals("qu")) {
+            return "Qu";
+        }
+
         return value.toUpperCase(getLocale());
     }
 
     @Override
     public String applyMandatorySuffix(String value) {
+        if (value.equals("q")) {
+            return "qu";
+        }
+
         return value;
     }
 
