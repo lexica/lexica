f693a7aba7bb3e5352fa4441f61effe18c6a265d
Peter Serwylo <peter@serwylo.com>
2018-05-08 15:24:10 +1000

    Added Scrabble scoring for German language.

--- libraries/trie/src/main/java/com/serwylo/lexica/lang/DeGerman.java
+++ libraries/trie/src/main/java/com/serwylo/lexica/lang/DeGerman.java
@@ -9,38 +9,46 @@ public class DeGerman extends Language {
     private static Map<String, Integer> letterPoints = new HashMap<>();
 
     static {
-        letterPoints.put("ß", 1);
-        letterPoints.put("à", 1);
-        letterPoints.put("a", 1);
-        letterPoints.put("b", 1);
-        letterPoints.put("c", 1);
-        letterPoints.put("d", 1);
-        letterPoints.put("ä", 1);
         letterPoints.put("e", 1);
-        letterPoints.put("f", 1);
-        letterPoints.put("g", 1);
-        letterPoints.put("h", 1);
-        letterPoints.put("i", 1);
         letterPoints.put("é", 1);
-        letterPoints.put("j", 1);
-        letterPoints.put("k", 1);
-        letterPoints.put("l", 1);
-        letterPoints.put("m", 1);
         letterPoints.put("n", 1);
-        letterPoints.put("o", 1);
-        letterPoints.put("p", 1);
-        letterPoints.put("q", 1);
-        letterPoints.put("r", 1);
         letterPoints.put("s", 1);
+        letterPoints.put("i", 1);
+        letterPoints.put("r", 1);
         letterPoints.put("t", 1);
         letterPoints.put("u", 1);
-        letterPoints.put("ö", 1);
+        letterPoints.put("a", 1);
+        letterPoints.put("à", 1);
+        letterPoints.put("d", 1);
+
+        letterPoints.put("h", 2);
+        letterPoints.put("g", 2);
+        letterPoints.put("l", 2);
+        letterPoints.put("o", 2);
+
+        letterPoints.put("m", 3);
+        letterPoints.put("b", 3);
+        letterPoints.put("w", 3);
+        letterPoints.put("z", 3);
+
+        letterPoints.put("c", 4);
+        letterPoints.put("f", 4);
+        letterPoints.put("k", 4);
+        letterPoints.put("p", 4);
+
+        letterPoints.put("ä", 6);
+        letterPoints.put("j", 6);
+        letterPoints.put("ü", 6);
         letterPoints.put("v", 1);
-        letterPoints.put("w", 1);
-        letterPoints.put("x", 1);
-        letterPoints.put("y", 1);
-        letterPoints.put("z", 1);
-        letterPoints.put("ü", 1);
+
+        letterPoints.put("ö", 8);
+        letterPoints.put("x", 8);
+
+        letterPoints.put("y", 10);
+        letterPoints.put("q", 10);
+
+        // Not listed in https://en.wikipedia.org/wiki/Scrabble_letter_distributions#German
+        letterPoints.put("ß", 1);
     }
 
     @Override
