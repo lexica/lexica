5b89d89b763f8baf404d7b8b92573871e2ec05eb
Peter Serwylo <peter@serwylo.com>
2020-05-24 22:56:12 +1000

    Return more specific locales for English US and GB. Required for defaulting to the correct language.

--- libraries/trie/src/main/java/com/serwylo/lexica/lang/EnglishUS.java
+++ libraries/trie/src/main/java/com/serwylo/lexica/lang/EnglishUS.java
@@ -1,8 +1,15 @@
 package com.serwylo.lexica.lang;
 
+import java.util.Locale;
+
 public class EnglishUS extends English {
     @Override
     public String getName() {
         return "en_US";
+    }
+
+    @Override
+    public Locale getLocale() {
+        return new Locale("en", "US");
     }
 }
